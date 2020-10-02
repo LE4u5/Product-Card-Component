b60ed908525c5f055eb834d3acd6d67b3869ab6f
LE4u5 <tdav06@yahoo.com>
2020-10-02 15:53:32 -0400

    style changes and added event listener for button

---
+++ README.md
@@ -0,0 +1,21 @@
+## PRODUCT CARD COMPONENT
+
+Import to html: <script src='./product_card_component.js'></script>
+
+### HTML Element Tag: 
+<product-card></product-card>
+
+### Inner HTML: 
+inner html is displayed as button text.
+
+### Attributes:
+    1. img: path of product image file
+    2. main: main text display. (exp. product name)
+    3. sub-one: first sub text.(exp. product price)
+    4. button-color: sets background color of button.
+    5. button-text-color: sets color of button text.
+    6. id: unique id for each card created. passes id to event listener detail.
+
+### Event Listener:
+listen for "product-card-clicked" to respond to button on click.
+
--- product_card_component.js
+++ product_card_component.js
@@ -10,8 +10,9 @@ class ProductCardComponent extends HTMLElement{
         this.subTwo =this.getAttribute('sub-two'); 
         this.buttonColor =this.getAttribute('button-color');
         this.buttonTextColor =this.getAttribute('button-text-color');
-        this.buttonText =this.getAttribute('button-text');
-
+        this.buttonText =this.innerHTML;
+        this.id= this.getAttribute('id');
+        
         this.template.innerHTML =  `
         <style>
             p{
@@ -25,7 +26,7 @@ class ProductCardComponent extends HTMLElement{
                 border-radius: .5rem;
                 border: none;
                 background-color: ${this.buttonColor} ;
-                font-weight: bold;
+                font-weight: 300;
                 font-size: .7rem;
                 color: ${this.buttonTextColor};
             }
@@ -38,7 +39,7 @@ class ProductCardComponent extends HTMLElement{
                 font-size: .98rem;
                 position: relative;
                 background-color: none;
-                width: 16rem;
+                width: 16.3rem;
                 height: 16rem;
                 transition: 500ms;
                 color: white;
@@ -53,7 +54,7 @@ class ProductCardComponent extends HTMLElement{
             .info-cont{
                 position: absolute;
                 background-color: black;
-                width: 13.5rem;
+                width: 11.8rem;
                 height: 1.2rem;
                 top: 13.75rem;
                 left: 2.5rem;
@@ -65,7 +66,6 @@ class ProductCardComponent extends HTMLElement{
             .top-info-cont{
                 display: flex;
                 justify-content: center;
-                
                 min-height: 50%;
                 width: 100%;
             }
@@ -77,22 +77,26 @@ class ProductCardComponent extends HTMLElement{
                 width: 100%;
             }
             .main-cont{
+                display: flex;
+                flex-wrap: nowrap;
                 font-weight: 700;
                 text-align: left;
-                width: 50%;
+                width: 70%;
                
             }
             .sub1-cont{
                 font-size: .8rem;
                 text-align: right;
-                width: 50%;
+                width: 30%;
                 
             }
             .card-cont:hover{
-                transform: scale(1.15,1.15);
+                transform: scale(1.05,1.05);
+                z-index: 100;
             }
             .card-cont:hover > .img-cont{
                 border-radius: 1rem;
+                box-shadow: 30px 30px 50px rgba(1,1,1,.3);
             }
             .card-cont:hover > .info-cont{
                 height: 5.5rem;
@@ -107,8 +111,12 @@ class ProductCardComponent extends HTMLElement{
             button:active{
                 transform: scale(1.1,1.1);
             }
+            .card-cont:focus{
+                outline: 1px solid black;
+                outline-offset: .5rem;
+            }
         </style>
-         <div class='card-cont'>
+         <div class='card-cont' tabindex='0'>
             <div class='img-cont'>
             <img src='${this.img}' alt='${this.main} Image'/>
             </div>
@@ -126,6 +134,22 @@ class ProductCardComponent extends HTMLElement{
 
         this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+    }
+
+    buttonClicked(){
+        const event = new CustomEvent('product-card-clicked',{
+            detail: {
+                id: this.id
+            }
+        });
+        document.dispatchEvent(event);
+    }
+
+    connectedCallback(){
+        this.shadowRoot.querySelector('button').addEventListener('click',()=> this.buttonClicked());
+    }
+    disconnectedCallback(){
+        this.shadowRoot.querySelector('button').removeEventListener();
     }
 }
 
